3292ecca7a696dabc771e3bde7dc07669073f6bc
ramdas <ramdasinfoinzenjer@gmail.com>
2017-10-07 17:19:32 +0530

    Initial commit

---
+++ app/src/main/java/info/androidhive/recyclerview/clickk.java
@@ -0,0 +1,12 @@
+package info.androidhive.recyclerview;
+
+import android.view.View;
+
+/**
+ * Created by SUDHEESH on 10/7/2017.
+ */
+
+public interface clickk {
+
+    public void abv(View v,int pos,String key);
+}
